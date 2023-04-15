ba28ddc1d359149c38bd27006cef3037eeef0d3b
Benedek Jokay <benedek.jokay@outlook.com>
2023-04-15 12:35:41 +0200

    benedek tanul gitet

--- src/Cso.java
+++ src/Cso.java
@@ -1,12 +1,20 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Cso {
+/**
+ * A játékban levo Csöveket reprezentáló osztály.
+ */
+public class Cso extends Mezo{
+    /**
+     * A szomszédos csúcsok listája
+     */
     private List<Csucs> szomszedosCsucs;
 
     public Cso(){
-
+        super();
+        szomszedosCsucs = new ArrayList<>();
 	
 
     }
+
 }
