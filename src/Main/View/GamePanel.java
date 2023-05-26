985d97ad1a0d3095f2c0169f8309c5d9db412c03
PencMark <pencmark@gmail.com>
2023-05-26 20:47:40 +0200

    PumpaView draw metodusa kesz.

--- src/Main/View/GamePanel.java
+++ src/Main/View/GamePanel.java
@@ -5,6 +5,7 @@ import Main.Model.Cso;
 import Main.Model.Csucs;
 import Main.Model.Jatekos;
 import Main.Model.Pumpa;
+import Main.Vec2;
 
 import javax.swing.*;
 import java.awt.*;
@@ -14,12 +15,19 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * Displays the game in a panel.
+ * A játék kirajzolásáért felelős panel.
  */
 public class GamePanel extends JPanel {
 
+    /**
+     * A singelton instance-je a GamePanelnek.
+     */
     private static GamePanel single_instance = null;
 
+    /**
+     * Singleton osztály, hogy csak egyetlen GamePanel objektum létezzen.
+     * @return A GamePanel objektum.
+     */
     public static synchronized GamePanel getInstance()
     {
         if (single_instance == null)
@@ -27,12 +35,24 @@ public class GamePanel extends JPanel {
 
         return single_instance;
     }
+
+    /**
+     * A játékosok nézeteit tartalmazó lista.
+     */
     List<JatekosView> jatekosok;
+
+    /**
+     * A csövek nézeteit tartalmazó lista.
+     */
     List<CsoView> csovek;
+
+    /**
+     * A csúcsok nézeteit tartalmazó lista.
+     */
     List<CsucsView> csucsok;
 
     /**
-     * Default ctor.
+     * Default Konstruktora a Game Panelnek.
      */
     private GamePanel(){
         super();
@@ -40,8 +60,8 @@ public class GamePanel extends JPanel {
         csovek = new ArrayList<>();
         csucsok = new ArrayList<>();
         Pumpa p = new Pumpa();
-        p.setVanViz(false);
-        p.setRossz(false);
+        p.setVanViz(true);
+        p.setRossz(true);
         PumpaView pm = new PumpaView(p);
         pm.setX(400);
         pm.setY(400);
@@ -61,13 +81,16 @@ public class GamePanel extends JPanel {
         CsoView csv2 = new CsoView(cs2);
         csv2.setX1(400);
         csv2.setY1(400);
-        csv2.setX2(100);
+        csv2.setX2(300);
         csv2.setY2(100);
         csovek.add(csv2);
+
+        p.setBemenetiCso(1);
     }
+
     /**
-     * Paints the components.
-     * @param g Erre lehet majd rajzolgatni. Ezt kene a Draw() metodusokban atadni( Draw(Graphics g) )
+     * Kirajzolja a pályát. Ez a csővek, csúcsok és játékosok kirajzolását jelenti.
+     * @param g A grafikus objektum, amire rajzolunk.
      */
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -83,15 +106,25 @@ public class GamePanel extends JPanel {
         }
     }
 
+    /**
+     * Kiszámolja az összes nézetet és megjeleníti az összes nézetet.
+     */
     public void drawAll(){
         calculateAll();
         showAll();
     }
 
+    /**
+     * Megjeleníti az összes nézetet.
+     */
     public void showAll(){
         this.repaint();
     }
 
+    /**
+     * Minden nézetet újraszámol.
+     * A csúcsokat egszer, a csöveket kétszer.
+     */
     public void calculateAll(){
         for (CsucsView cs : csucsok){
             cs.calculate();
@@ -104,6 +137,11 @@ public class GamePanel extends JPanel {
         }
     }
 
+    /**
+     * Visszaadja a játékos nézetet a játékos alapján.
+     * @param j A játékos, aminek a nézetét keresi.
+     * @return A játékos nézete.
+     */
     public JatekosView getPlayerViewFromPlayer(Jatekos j){
         for (JatekosView jv : jatekosok){
             if (jv.getJatekos() == j){
@@ -113,6 +151,11 @@ public class GamePanel extends JPanel {
         return null;
     }
 
+    /**
+     * Visszaadja a csúcs nézetet a csúcs alapján.
+     * @param c A csúcs, aminek a nézetét keresi.
+     * @return A csúcs nézete.
+     */
     public CsucsView getCsucsViewFromCsucs(Csucs c){
         for (CsucsView cv : csucsok){
             if (cv.getCsucs() == c){
@@ -122,6 +165,11 @@ public class GamePanel extends JPanel {
         return null;
     }
 
+    /**
+     * Visszaadja mindazon játékosokat, akik a csövet cipelik.
+     * @param c A cső, aminek a cipelőit keresi.
+     * @return A csövet cipelő játékosok listája.
+     */
     public List<JatekosView> getPlayerViewFromCso(Cso c){
         List<JatekosView> jatekosok = new ArrayList<>();
         for (JatekosView jv : this.jatekosok){
@@ -132,6 +180,11 @@ public class GamePanel extends JPanel {
         return jatekosok;
     }
 
+    /**
+     * Visszaadja a megadott csőhöz tartozó CsoView-t.
+     * @param c A cső, aminek a CsoView-ját keresi.
+     * @return A csőhöz tartozó CsoView.
+     */
     public CsoView getCsoViewFromCso(Cso c){
         for (CsoView cv : csovek){
             if (cv.getCso() == c){
@@ -139,6 +192,28 @@ public class GamePanel extends JPanel {
             }
         }
         return null;
+    }
+
+    /**
+     * Kirajzol egy háromszöget a megadott helyre, a megadott irányba.
+     * @param g Graphics, amire rajzolni kell.
+     * @param from Az egyenlő szárú háromszög rövidebb oldalának a középpontja.
+     * @param dir Az egyenlő szárú háromszög rövidebb oldalának a közeppontjából a szembe lévő csúcsba mutató vektor, ezzel arányos lesz a háromszög elnyúlása.
+     */
+    public void drawTriangle(Graphics g, Vec2 from, Vec2 dir){
+        int size = 8;
+        Vec2 a;
+        Vec2 b;
+        Vec2 c;
+
+        a = from.add(dir.multiply(size));
+        Vec2 oldal = dir.getPerpendicular().normalize();
+        b = from.add(oldal.multiply(size));
+        c = from.add(oldal.multiply(-size));
+
+        int x[] = {(int)a.getX(),(int)b.getX(),(int)c.getX()};
+        int y[] = {(int)a.getY(),(int)b.getY(),(int)c.getY()};
+        g.fillPolygon(x,y,3);
     }
 }
 
--- src/Main/View/PumpaView.java
+++ src/Main/View/PumpaView.java
@@ -8,21 +8,47 @@ import Main.Vec2;
 import java.awt.*;
 import java.util.List;
 
+/**
+ * A pumpa kirajzolásáért felelős osztály.
+ */
 public class PumpaView extends CsucsView{
+    /**
+     * A kirajzolandó pumpa.
+     */
     private Pumpa pumpa;
 
-    private int radius = 40;
+    /**
+     * A pumpa sugara.
+     */
+    private int radius = 30;
 
+    /**
+     * Default ctor.
+     * @param p a pumpa amit ki kell rajzolni.
+     */
     public PumpaView(Pumpa p){
         pumpa = p;
     }
+
+    /**
+     * Kirajzolja a pumpát.
+     * A pumpa sugara a radius. Ezzel lehet a meretet allítani.
+     * Ha a pumpa rossz, akkor piros keretet rajzol a pumpa köré.
+     * Ha a pumpában van víz, akkor kék a pumpa, különben fekete.
+     * A szomszédos csövek számát a pumpa szélére írja.
+     * A kimeneti cső irányába egy nyilat rajzol a pumpán kívülre.
+     * A bemeneti cső irányába egy nyilat rajzol a pumpán kívülre.
+     * @param g
+     */
     @Override
     public void draw(Graphics g){
         int widthOfRossz = 10;
+        //Kirajzolja a pumpa keretét.
         if (pumpa.getRossz()){
             g.setColor(Color.RED);
             g.fillOval(x-radius-widthOfRossz/2,y-radius-widthOfRossz/2,2*radius+widthOfRossz,2*radius+widthOfRossz);
         }
+        //Kirajzolja a pumpa körét, ha van benne víz, akkor kék, különben fekete.
         if(pumpa.getVanViz()){
             g.setColor(Color.BLUE);
         }
@@ -32,6 +58,7 @@ public class PumpaView extends CsucsView{
         g.fillOval(x-radius,y-radius,2*radius,2*radius);
 
         g.setColor(Color.WHITE);
+
         //Kirajzolja a szomszedos csovek szamat.
         List<Cso> szomszedoscsovek = pumpa.getSzomszedosCso();
         for (int i=0; i<szomszedoscsovek.size(); i++){
@@ -44,12 +71,42 @@ public class PumpaView extends CsucsView{
             direction = direction.multiply(radius-10);
             g.drawString(Integer.toString(i+1),(int)(getX()+direction.getX()),(int)(getY()+direction.getY()));
         }
+
+        //kirajzolja a kimeno cso iranyat
+        g.setColor(Color.BLACK);
+        Cso kimeneti = szomszedoscsovek.get(pumpa.getKimenetiCso());
+        CsoView kimenetiView = GamePanel.getInstance().getCsoViewFromCso(kimeneti);
+        Vec2 direction = new Vec2(kimenetiView.getX1()-getX(),kimenetiView.getY1()-getY());
+        if (kimenetiView.getX1() == getX() && kimenetiView.getY1() == getY()) {
+            direction = new Vec2(kimenetiView.getX2() - getX(), kimenetiView.getY2() - getY());
+        }
+        direction = direction.normalize();
+
+        GamePanel.getInstance().drawTriangle(g,new Vec2(getX(),getY()).add(direction.multiply(radius+10)),direction.multiply(3));
+
+        //kirajzolja a bemeneti cso iranyat
+        Cso bemeneti = szomszedoscsovek.get(pumpa.getBemenetiCso());
+        CsoView bemenetiView = GamePanel.getInstance().getCsoViewFromCso(bemeneti);
+        Vec2 direction2 = new Vec2(bemenetiView.getX1()-getX(),bemenetiView.getY1()-getY());
+        if (bemenetiView.getX1() == getX() && bemenetiView.getY1() == getY()) {
+            direction2 = new Vec2(bemenetiView.getX2() - getX(), bemenetiView.getY2() - getY());
+        }
+        direction2 = direction2.normalize();
+
+        GamePanel.getInstance().drawTriangle(g,new Vec2(getX(),getY()).add(direction2.multiply(radius+40)),direction2.multiply(-3));
     }
 
+    /**
+     * Kiszámolja a rajta álló játékosok helyzetét.
+     */
     @Override
     public void calculate(){
     }
 
+    /**
+     * Visszaadja a pumpát amit kirajzol
+     * @return a pumpa, aminek a kirajzolásáért felelős.
+     */
     @Override
     public Csucs getCsucs(){
         return pumpa;
