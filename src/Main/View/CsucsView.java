532ab0b1cade0184489680d9d5c3fc2527b2a443
PencMark <pencmark@gmail.com>
2023-05-26 18:36:21 +0200

    addedgettersetter

--- src/Main/View/CsucsView.java
+++ src/Main/View/CsucsView.java
@@ -6,6 +6,22 @@ public abstract class CsucsView {
     int x;
     int y;
 
+    public int getY() {
+        return y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setY(int y){
+        this.y=y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
     public abstract void draw(Graphics g);
     public abstract void calculate();
 }
--- src/Main/View/JatekosView.java
+++ src/Main/View/JatekosView.java
@@ -5,6 +5,23 @@ import java.awt.*;
 public abstract class JatekosView {
     private int x;
     private int y;
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public void setX(int x){
+        this.x=x;
+    }
+
+    public void setY(int y){
+        this.y=y;
+    }
+
     public abstract void draw(Graphics g);
     public abstract void calculate();
 }
