8fbb51fa4f1823bbaa2418f0a8ce97f96dbda016
PencMark <pencmark@gmail.com>
2023-05-26 19:26:34 +0200

    getters

--- src/Main/View/CsoView.java
+++ src/Main/View/CsoView.java
@@ -11,6 +11,22 @@ public class CsoView {
     private int x2;
     private int y2;
 
+    public int getY1() {
+        return y1;
+    }
+
+    public int getX1() {
+        return x1;
+    }
+
+    public int getY2() {
+        return y2;
+    }
+
+    public int getX2() {
+        return x2;
+    }
+
     //default ctor
     CsoView(){
     }
